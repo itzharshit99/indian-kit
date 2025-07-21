24175817b43a22844f7a573d964e8381d84add2d
Harshit Jain <iharshitjain99@gmail.com>
2025-07-21 23:05:06 +0530

    added pan validator functionality with ocr based authentication

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const { parseIndianAddress } = require('./addressParser');
 const { validateIndianPhone } = require('./phoneValidator');
 const {validatePincode} = require('./pincodeValidator')
 const {geocodeAddress} = require('./geocoder')
+const {validatePanCard} = requiure('./panValidator.js');
 module.exports = {
   parseIndianAddress,
   validateIndianPhone,
   validatePincode,
-  geocodeAddress
+  geocodeAddress,
+  validatePanCard
 };
