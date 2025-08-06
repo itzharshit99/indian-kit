d802e24eecf9515d96a01b1517e24036868f8209
Harshit Jain <iharshitjain99@gmail.com>
2025-08-06 15:05:17 +0530

    minor issues fixed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const { parseIndianAddress } = require('./addressParser');
 const { validateIndianPhone } = require('./phoneValidator');
 const {validatePincode} = require('./pincodeValidator')
 const {geocodeAddress} = require('./geocoder')
-const {validatePanCard} = requiure('./panValidator.js');
+// const {validatePanCard} = requiure('./panValidator.js');
 module.exports = {
   parseIndianAddress,
   validateIndianPhone,
   validatePincode,
-  geocodeAddress,
-  validatePanCard
+  geocodeAddress
 };
